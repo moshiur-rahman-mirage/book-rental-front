6399a9ecd3cef2e32014d34b4a4772747c54ee52
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-11-13 21:34:41 +0600

    vercel link added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosSecure=axios.create({
-    baseURL:'http://localhost:5000',
+    baseURL:'https://book-rental-back.vercel.app',
     withCredentials:false
 })
 
